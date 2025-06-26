4746670fb0163635b65a1d6438c604aaa8ba4634
David Moreno Gamez <dmorgam94@gmail.com>
2025-06-26 11:22:32 +0200

    fix rest nvim

--- config/nvim/lua/plugins/common.lua
+++ config/nvim/lua/plugins/common.lua
@@ -31,14 +31,26 @@ return {
   -- Rest Api testing
   {
     'rest-nvim/rest.nvim',
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function (_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          table.insert(opts.ensure_installed, "http")
+        end,
+    },
     config = function ()
-
-      require("rest-nvim").setup({
-        default_config = {
-          skip_ssl_verification = true
+        vim.g.rest_nvim = {
+            request = {
+                skip_ssl_verification = true
+            }
         }
-      })
 
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "json",
+            callback = function()
+                vim.opt_local.formatprg = "jq ."
+            end
+        })
     end
   },
 }
