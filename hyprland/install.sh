2b6961a747cf6c2929a83ba3dba9046e5d0dbf11
David Moreno Gamez <dmorgam94@gmail.com>
2025-07-06 20:14:24 +0200

    suspend only on battery

--- hyprland/install.sh
+++ hyprland/install.sh
@@ -39,7 +39,7 @@ then
           xdg-desktop-portal-hyprland thunar thunar-archive-plugin xarchiver
           pipewire wireplumber mako libnotify ttf-jetbrains-mono-nerd hyprpolkitagent
           python-pillow highlight ttf-nerd-fonts-symbols wl-clipboard udiskie
-          xdg-user-dirs"
+          xdg-user-dirs acpi"
     AUR_DEPS="wlogout"
 
     sudo pacman -S --needed $DEPS
