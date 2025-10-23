c07660a6474bdd21b51e52fb01141edd3e04efcb
David Moreno Gamez <dmorgam94@gmail.com>
2025-10-23 10:27:07 +0200

    use lsp native api

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,11 @@ return {
     'neovim/nvim-lspconfig',
     config = function ()
 
-      require('lspconfig').lua_ls.setup {
+      -- cmp integration
+      local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
         settings = {
           Lua = {
             runtime = { version = 'LuaJIT' },
@@ -14,29 +18,38 @@ return {
             telemetry = { enable = false },
           },
         },
-      }
-
-      -- cmp integration
-      local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+      })
 
       -- LSP servers
       local servers = { "pylsp", "ts_ls", "terraformls",
                         "yamlls", "jsonls" }
 
       for _, server in ipairs(servers) do
-          require('lspconfig')[server].setup {
-            capabilities = capabilities
+          vim.lsp.config[server] = {
+              capabilities = capabilities,
           }
       end
 
       -- Powershell LSP
-      require("lspconfig").powershell_es.setup {
-          bundle_path = vim.fn.expand("$HOME") .. "/.local/share/nvim/mason/packages/powershell-editor-services/",
-          init_options = {
-              enableProfileLoading = false,
+      vim.lsp.config.powershell_es = {
+          capabilities = capabilities,
+          cmd = {
+            vim.fn.expand("$HOME") ..
+            "/.local/share/nvim/mason/packages/powershell-editor-services/PowerShellEditorServices.Host",
           },
+          init_options = {
+            enableProfileLoading = false,
+          }
       }
 
+      -- Enable servers
+      vim.lsp.enable(vim.lsp.config.lua_ls)
+
+      for _, server in ipairs(servers) do
+        vim.lsp.enable(vim.lsp.config[server])
+      end
+
+      vim.lsp.enable(vim.lsp.config.powershell_es)
 
     end
   }
